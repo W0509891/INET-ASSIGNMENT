bd1d04d580598eb24dc5099a5c8718b039dda8ac
W0509891 <W0509891@campus.nscc.ca>
2025-10-02 18:07:39 -0300

    idrk

---
+++ PASSIFY/PASSIFY/Data/PASSIFYContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PASSIFY.Models;
+
+namespace PASSIFY.Data
+{
+    public class PASSIFYContext : DbContext
+    {
+        public PASSIFYContext (DbContextOptions<PASSIFYContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<PASSIFY.Models.Category> Category { get; set; } = default!;
+    }
+}
