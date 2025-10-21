172c66cb3e51f3e65518eadb06d89a280f963936
niyi-16 <dasil.adam@gmail.com>
2025-10-20 21:59:02 -0300

    Controller for extra functions

---
+++ PASSIFY/Controllers/ExtraController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace PASSIFY.Controllers;
+
+public class ExtraController : Controller
+{
+    // GET
+    public IActionResult LastPage()
+    {
+        return Redirect(Request.Cookies["LastPage"]);
+    }
+}
