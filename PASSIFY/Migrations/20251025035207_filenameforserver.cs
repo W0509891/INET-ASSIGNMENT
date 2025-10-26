e787fdf8480842a17b054c055a3f4af3a1508148
niyi-16 <dasil.adam@gmail.com>
2025-10-26 03:42:09 -0300

    Migrations

---
+++ PASSIFY/Migrations/20251025035207_filenameforserver.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PASSIFY.Migrations
+{
+    /// <inheritdoc />
+    public partial class filenameforserver : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "EventImageName",
+                table: "Activity",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EventImageName",
+                table: "Activity");
+        }
+    }
+}
