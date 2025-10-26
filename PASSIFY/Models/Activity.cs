83bbd3dae1d9fe5ac7389ff804e3354b668e3fbf
niyi-16 <dasil.adam@gmail.com>
2025-10-26 03:41:30 -0300

    Added image and unified date fromat

--- PASSIFY/Models/Activity.cs
+++ PASSIFY/Models/Activity.cs
@@ -1,40 +1,46 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.VisualBasic;
 
 namespace PASSIFY.Models;
 
 public class Activity
 {
- 
+    //String for date format
+    private const string DateFormat = "{0:dd-MMM-yy hh:mm tt}";
     public int ActivityId { get; set; } //Primary Key
 
     public string Title { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
 
-    [Display(Name = "Start"), 
-     DisplayFormat(DataFormatString = "{0:dd-MMM-yy hh:mm tt}")]
+    [Display(Name = "Start"),
+     DisplayFormat(DataFormatString = DateFormat)]
     public DateTime EventStart { get; set; }
 
     [Display(Name = "End"),
-     DisplayFormat(DataFormatString = "{0:dd-MMM-yy hh:mm tt}")]
+     DisplayFormat(DataFormatString = DateFormat)]
     public DateTime EventEnd { get; set; }
 
-    [DisplayFormat(DataFormatString = "{0:dd-MMM-yy hh:mm tt}")]
-    public DateTime Created { get; set; } = DateTime.Now;
-    
-    [DisplayFormat(DataFormatString = "{0:dd-MMM-yy hh:mm tt}")]
+    [DisplayFormat(DataFormatString = DateFormat)]
+    public DateTime Created { get; set; }
+
+    [DisplayFormat(DataFormatString = DateFormat)]
     public DateTime Modified { get; set; }
 
-    // Foreign Keys
+    [NotMapped]
+    [Display(Name = "Image")]
+    public IFormFile? EventImage { get; set; }
+    
+    //Name of Image on server
+    public string? EventImageName { get; set; }
 
-    [Display(Name = "Organizer")]
-    public int? OrganizerId { get; set; }
+    // ==== Foreign Keys ====//
 
-    [Display(Name = "Category")]
-    public int? CategoryId { get; set; } 
+    [Display(Name = "Organizer")] public int? OrganizerId { get; set; }
 
-    // Navigation Properties
+    [Display(Name = "Category")] public int? CategoryId { get; set; }
+
+    // ==== Navigation Properties ==== //
     public Organizer? Organizer { get; set; }
     public Category? Category { get; set; }
-    
 }
-
