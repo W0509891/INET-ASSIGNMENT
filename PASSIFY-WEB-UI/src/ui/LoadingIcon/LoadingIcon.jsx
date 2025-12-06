e70cf9ac2fb13ecb5377c9c789870bc713d876b8
niyi-16 <dasil.adam@gmail.com>
2025-12-06 18:15:54 -0400

    Loadinv icon

---
+++ PASSIFY-WEB-UI/src/ui/LoadingIcon/LoadingIcon.jsx
@@ -0,0 +1,10 @@
+import "./LoadingIcon.scss"
+function LoadingIcon() {
+
+    // From https://www.w3schools.com/howto/howto_css_loader.asp
+    return (
+        <div className="loader"></div>
+    )
+}
+
+export default LoadingIcon
