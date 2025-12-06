e572573bb32f96f72facb53cf3b2bfea66970c95
niyi-16 <dasil.adam@gmail.com>
2025-12-06 18:08:27 -0400

    Switched to SCSS for styling, added Details and Purchase routes, and updated navigation structure.

--- PASSIFY-WEB-UI/src/main.jsx
+++ PASSIFY-WEB-UI/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './index.scss'
 //Router
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
@@ -11,6 +11,8 @@ import Navbar from "./ui/Navbar/Navbar.jsx";
 import App from './routes/App.jsx'
 import Event from './routes/Event/Event.jsx';
 import Ticket from './routes/Ticket/Ticket.jsx';
+import Details from "./routes/Details/Details.jsx";
+import Purchase from "./routes/Purchase/Purchase.jsx";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -18,6 +20,8 @@ createRoot(document.getElementById('root')).render(
       <Navbar />
          <Routes>
              <Route path="/" element={<App />} />
+             <Route path="/details/:id" element={<Details />} />
+             <Route path="/Purchase" element={<Purchase />} />
              <Route path="/event" element={<Event />} />
              <Route path="/tickets" element={<Ticket />} />
          </Routes>
