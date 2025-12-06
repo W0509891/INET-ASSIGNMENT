81b82c92fbb8a2a8c78715ec24567ffe1a502231
niyi-16 <dasil.adam@gmail.com>
2025-12-06 18:09:22 -0400

    Integrated event grid layout with dynamic content, implemented `useEffect` for activity fetching, and replaced placeholder content in `App.jsx`. Adjusted styling in `App.css` for responsive design.

--- PASSIFY-WEB-UI/src/routes/App.jsx
+++ PASSIFY-WEB-UI/src/routes/App.jsx
@@ -1,35 +1,51 @@
-import { useState } from 'react'
-import reactLogo from '../assets/react.svg'
-import viteLogo from '/vite.svg'
+import {useEffect, useState} from 'react'
 import './App.css'
+//Components
+import EventCard from "../ui/EventCard/EventCard.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [activities, setActivities] = useState([])
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const apiUrl = import.meta.env.VITE_API_URL
+
+    useEffect(() => {
+        const getActivities = async () => {
+            const response = await fetch(apiUrl + '')
+            const data = await response.json()
+
+            if (response.ok) {
+                setActivities(data)
+            }
+        }
+        getActivities()
+    }, [])
+
+
+
+
+    return (
+        <>
+            <div className="event-grid-container">
+                {
+                    activities.length > 0 && (
+                        activities.map(activity =>(
+                            <EventCard
+                                key={activity.ActivityId}
+                                ActivityId={activity.ActivityId}
+                                ActivityName={activity.ActivityName}
+                                ImageName={activity.ImageName}
+                                Description={activity.Description}
+                                EventStart={activity.EventStart}
+                                EventEnd={activity.EventEnd}
+                                Organizer={activity.Organizer}
+                            />
+                        ))
+                    )
+                }
+            </div>
+
+        </>
+    )
 }
 
 export default App
