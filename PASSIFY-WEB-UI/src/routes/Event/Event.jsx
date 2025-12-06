6e16bb2575b10721162003e44b6fd5a6a99c255a
niyi-16 <dasil.adam@gmail.com>
2025-12-06 18:10:17 -0400

    Added introductory text and external link for event hosting instructions in Event component

--- PASSIFY-WEB-UI/src/routes/Event/Event.jsx
+++ PASSIFY-WEB-UI/src/routes/Event/Event.jsx
@@ -1,8 +1,14 @@
+import {Link} from "react-router-dom";
+
 function Event() {
 
     return (
         <>
             <h1>Events</h1>
+
+            <p>Hello there, Looking to host an event?</p>
+            <p>You would need to create an account with us and we can get you started</p>
+            <a href={"https://www.example.com"} target={"_blank"}>Create and account</a>
         </>
     )
 }
