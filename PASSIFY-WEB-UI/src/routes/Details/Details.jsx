b3cca9cf2280ccd1f9cac7449ac10e33cbba4b2e
niyi-16 <dasil.adam@gmail.com>
2025-12-06 18:11:59 -0400

    Added Details route

---
+++ PASSIFY-WEB-UI/src/routes/Details/Details.jsx
@@ -0,0 +1,81 @@
+import './Details.scss'
+import {Link, useParams} from "react-router-dom";
+import {useEffect, useState} from "react";
+
+function Details() {
+    const {id} = useParams();
+
+    const [activity, setActivity] = useState(null)
+
+    const apiUrl = import.meta.env.VITE_API_URL
+
+    useEffect(() => {
+        const getActivity = async () => {
+            const response = await fetch(`${apiUrl}/${id}`)
+            const data = await response.json()
+
+            if (response.ok) {
+                setActivity(data)
+            }
+        }
+        getActivity()
+    }, [])
+
+    return (
+        <>
+
+            <div className={"details-container"}>
+
+                <div className="details-header">
+                    <Link to={'/'} className="btn btn-outline-light btn-sm">← Back to home</Link>
+                </div>
+
+                {activity &&
+                <div className={"details-content"}>
+                    <h2 className="details-title">{activity.ActivityName}</h2>
+                    <p>{activity.Description}</p>
+                    <img className="details-hero" src={activity.ImageName} alt={activity.ActivityName}/>
+
+                    <div className={"details-additional"}>
+
+
+                        <div>
+                            <p>Starts: </p>
+                            <p>{activity.EventStart}</p>
+                        </div>
+
+                        <div>
+                            <p>Ends:</p>
+                            <p>{activity.EventEnd}</p>
+                        </div>
+
+                        <div>
+                            <p>Organized by:</p>
+                            <p>{activity.Organizer}</p>
+                        </div>
+
+                        <div>
+                            <p>Category:</p>
+                            <p>{activity.Category}</p>
+                        </div>
+
+                    </div>
+
+                </div>}
+            </div>
+
+            {activity && (
+                <div className="details-cta">
+                    <Link to={`/Purchase?event_name=${activity.ActivityName}&event_id=${id}`}
+                          className="btn btn-ticket">
+                        Buy Ticket
+                    </Link>
+                </div>
+            )}
+
+        </>
+
+    )
+}
+
+export default Details
