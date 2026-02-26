3c791cbac61c2383dea63b3503fb92fe415b1797
niyi-16 <dasil.adam@gmail.com>
2026-02-25 21:18:10 -0400

    user service file

---
+++ PASSIFY-WEB-API/services/UserService.js
@@ -0,0 +1,40 @@
+import {MongoClient} from 'mongodb'
+import 'dotenv/config.js'
+
+const client = new MongoClient(process.env.MONGO_CONN)
+
+const getUser = async (user_email, password) => {
+    try {
+        await client.connect()
+        const db = client.db('passify')
+        const collecton = db.collection('users')
+        const query = {email: user_email, password: password}
+        const projection = {_id: 0, password: 0}
+        const result =  await collecton.findOne(query, {projection: projection})
+        return result
+    }
+    catch (e) {
+        console.error(e)
+    }
+    finally {
+        await client.close()
+    }
+}
+
+const createUser = async (data) => {
+    await client.connect()
+    console.log(data)
+    const db = client.db('passify')
+    const collection = db.collection('users')
+    const result = await collection.insertOne(data)
+    return result.acknowledged
+}
+
+const user = new Object({
+    first_name: String,
+    last_name: String,
+    email: String,
+    password: String,
+})
+
+export {getUser, createUser, user}
